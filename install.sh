40fb6d6f3d9ec294fe77af9b5dae90a10c1687de
DuckingDucking <maclinux793@gmail.com>
2025-02-19 07:43:05 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ pkg update -y && pkg upgrade -y
 pkg install tur-repo
 pkg install python3.11
 
-pip3.11 install requests httpx rich bs4
+pip3.11 install requests httpx rich bs4 mechanize madhix colorama
